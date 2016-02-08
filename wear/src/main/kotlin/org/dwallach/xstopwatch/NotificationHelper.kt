a76543e97662da670e6171b2d08e30c494056f67
Dan Wallach <dwallach@cs.rice.edu>
2016-02-08 12:28:53 -0600

    cleanup

--- wear/src/main/kotlin/org/dwallach/xstopwatch/NotificationHelper.kt
+++ wear/src/main/kotlin/org/dwallach/xstopwatch/NotificationHelper.kt
@@ -7,12 +7,10 @@
 package org.dwallach.xstopwatch
 
 import android.app.Notification
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
-import android.graphics.drawable.Icon
 import android.util.Log
 
 import java.util.Observable
